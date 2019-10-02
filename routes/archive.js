0a578720b9c484513acd4a5b7a741d6baf96bf76
Zoubeir M <44193946+Zoubir78@users.noreply.github.com>
2019-10-02 23:54:04 +0200

    Add files via upload

--- routes/archive.js
+++ routes/archive.js
@@ -1,16 +1,24 @@
 
 const express = require('express');
-
+const bodyParser = require("body-parser");
 const router = express.Router();
 
 console.log('dans le fichier archive.js corrigé');
 
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/2004-5.pug',function(req,res,next) {
+    res.render('archive/2004-5',{title:'AsF Evenements en 2019 - Transformations'});
+});
+ 
+router.post('/2004-5.pug', function(req,res,next) {
+    var p1 = request.body.p1; 
+    console.log("p1=" + p1);
+});
 
 router.get('/actuel',function(req,res,next) {
     console.log('dans la fonction actuel');
     res.render('archive/actuel',{title:'Artistes sans Frontières - Actualités '});
 });
-router.get('/2019-1',function(req,res,next) {
-    res.render('archive/2019-1',{title:'AsF Evenements en 2019 - Transformations'});
-});
+
 module.exports = router;
