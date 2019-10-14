ea457710a6aad3ac3ef1377214b23cdc1b47a978
helgaParis <helga@artistessansfrontieres.fr>
2019-10-14 20:07:37 +0200

    cleanup

---
+++ sources/images-galerie/base/amorim/dias-listes.js
@@ -0,0 +1,21 @@
+//url list, one list per show.
+
+var photos=[
+    "url('/images-galerie/base/amorim/huile-marche.jpg')",
+    "url('/images-galerie/base/amorim/marche-melons.jpg')",
+    "url('/images-galerie/base/amorim/pots.jpg')",
+    "url('/images-galerie/base/amorim/cruche.jpg')",
+    "url('/images-galerie/base/amorim/cuilleres.jpg')",
+    "url('/images-galerie/base/amorim/oignons.jpg')"
+];
+// sans titre: mettre ''
+var legendes = [
+    'Scène de marché, Les crevettes - camarões, huile sur toile - oleo sobre tela - oil on canvas - Öl auf Leinwand, 60 x 70 cm',
+    'Scène de marché 2, pasteques - melanciaas, huile sur toile - oleo sobre tela - oil on canvas - Öl auf Leinwand, 70 x 80 cm',
+    'Scène de marché - Caxixi, huile sur toile - oleo sobre tela - oil on canvas - Öl auf Leinwand, 60 x 70 cm',
+    'Nature morte, oignons et percil - cebolas e salsinha, huile sur toile - oleo sobre tela - oil on canvas - Öl auf Leinwand, 40 x 50 cm',
+    'Bouquet de cuillère - Bouquet de colheres - Bunch of spoons - Kochlöffelstrauss, huile sur toile - oleo sobre tela - oil on canvas - Öl auf Leinwand, 65 x 81cm',
+    'Nature morte, oignons et piments - cebolas e pimenta, huile sur toile - oleo sobre tela - oil on canvas - Öl auf Leinwand, 60 x 80 cm'
+];
+
+var delai = 4000; // delai between pictures in ms
